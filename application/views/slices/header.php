fcb08b977481c972f397a7dd18d1e37d50f04425
Pedro Ovidio <pedrovidio@gmail.com>
2020-11-13 18:20:11 -0300

    tablet view

--- application/views/slices/header.php
+++ application/views/slices/header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="HTML, CSS, PHP, JavaScript">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>INFINITA</title>
 	<link rel="shortcut icon" href="assets/img/icon.png">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
