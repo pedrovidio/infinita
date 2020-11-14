c31f12bf484774d70d20f45d3d945d517501d01a
augustosbrasil <57014881+augustosbrasil@users.noreply.github.com>
2020-11-14 10:31:07 -0300

    inclusão do icon no header

--- application/views/slices/header.php
+++ application/views/slices/header.php
@@ -6,7 +6,7 @@
 	<meta name="keywords" content="HTML, CSS, PHP, JavaScript">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>INFINITA</title>
-	<link rel="shortcut icon" href="assets/img/icon.png">
+	<link rel="shortcut icon" href="<?= base_url('assets/img/icon.png') ?>">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 	<?php
